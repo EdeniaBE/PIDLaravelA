9545632392f6f4d388171f96da28b679221aceb3
Unity-hunter\Gaming <ag-julien@outlook.com>
2021-04-01 22:04:36 +0200

    Controller Artist + templates + routes

--- reservations/routes/web.php
+++ reservations/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArtistController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artist', [ArtistController::class, 'index'])->name('artist_index');
+Route::get('/artist/{id}', [ArtistController::class, 'show'])
+	->where('id', '[0-9]+')->name('artist_show');
